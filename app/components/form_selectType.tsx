6796daa9f112f68fd388286aecc4682ae79733f4
douglas duarte <douglas_duart@outlook.com>
2025-09-22 15:40:00 -0300

    create form selecte type register

---
+++ app/components/form_selectType.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Dispatch, SetStateAction } from "react";
+
+export default function FormSelectType({
+  typeRegister,
+  setTypeRegister,
+}: {
+  typeRegister: "cpf" | "cnpj";
+  setTypeRegister: Dispatch<SetStateAction<"cpf" | "cnpj">>;
+}) {
+  return (
+    <div className="mb-6">
+      <p className="label mb-1">Tipo de cadastro</p>
+      <form className="flex items-center gap-x-8">
+        <label className="flex items-center gap-2">
+          <input
+            type="radio"
+            value="cpf"
+            checked={typeRegister === "cpf"}
+            onChange={() => setTypeRegister("cpf")}
+          />
+          CPF
+        </label>
+
+        <label className="flex items-center gap-2">
+          <input
+            type="radio"
+            value="cnpj"
+            checked={typeRegister === "cnpj"}
+            onChange={() => setTypeRegister("cnpj")}
+            className="h-max"
+          />
+          CNPJ
+        </label>
+      </form>
+    </div>
+  );
+}
