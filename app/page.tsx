959511824485f3d470bd8a4dcb05d3b880dd7bd4
douglas duarte <douglas_duart@outlook.com>
2025-09-22 15:40:28 -0300

    add state type register

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import FormData from "./components/form_data";
+"use client";
+
+import { useState } from "react";
+import FormSelectType from "./components/form_selectType";
+import FormCpfData from "./components/form_cpfData";
+import FormCnpjData from "./components/form_cnpjData";
 
 export default function Home() {
+  const [typeRegister, setTypeRegister] = useState<"cpf" | "cnpj">("cpf");
+
   return (
     <div className="w-full my-12 border-2 border-[#374151]  rounded-sm p-10 dark:bg-[#1F2937]">
-      <FormData />
+      <FormSelectType
+        typeRegister={typeRegister}
+        setTypeRegister={setTypeRegister}
+      />
+      {typeRegister === "cpf" && <FormCpfData />}
+      {typeRegister === "cnpj" && <FormCnpjData />}
     </div>
   );
 }
