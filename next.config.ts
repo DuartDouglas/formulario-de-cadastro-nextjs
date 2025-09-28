9cab130aab2d16c9e6c4b009ae7fbed848802b36
douglas duarte <douglas_duart@outlook.com>
2025-09-28 16:32:51 -0300

    add git hub pages

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: "export", // necessário para rodar no GitHub Pages
-  basePath: isProd ? "/NOME-DO-REPO" : "",
-  assetPrefix: isProd ? "/NOME-DO-REPO/" : "",
-  images: {
-    unoptimized: true, // github pages não tem suporte nativo a Image Optimization
-  },
+  /* config options here */
 };
 
 export default nextConfig;
